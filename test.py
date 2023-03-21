9e07b8e3d2cfce0e5c5c45b2a0584f98fefdb271
priya <shunmugapriya530@gmail.com>
2023-03-21 07:50:18 +0530

    multiplication program

---
+++ test.py
@@ -0,0 +1,6 @@
+def mult(a,b):
+    return a*b
+num1=int(input("Enter the value of num1:"))
+num2=int(input("Enter the value of num2:"))
+value=mult(num1,num2)
+print("My multiplication value is "+str(value))
