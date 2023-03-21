40bfe7560f832f7718341d2f188677bbc749898b
priya <shunmugapriya530@gmail.com>
2023-03-21 09:18:57 +0530

    my calculation

---
+++ oper.py
@@ -0,0 +1,6 @@
+def greater_number_of_three(a,b,c):
+    if a>b and a>c:
+        print("The ")
+        
+var=greater_number_of_three(7,7,7)
+print("The equal num is :"+str(var)) 
--- test.py
+++ test.py
@@ -4,3 +4,4 @@ num1=int(input("Enter the value of num1:"))
 num2=int(input("Enter the value of num2:"))
 value=mult(num1,num2)
 print("My multiplication value is "+str(value))
+
