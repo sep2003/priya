93864bc4bc962cbc9ff502378493a2bc2ba658e4
priya <shunmugapriya530@gmail.com>
2023-03-22 09:08:17 +0530

    my program

---
+++ test8.py
@@ -0,0 +1,14 @@
+def number(n):
+    if (n<=100):
+        print("the number is between 0 and 100.")
+        
+    elif (n<=1000):
+        print("the number is between 100 and 1000.")
+    
+    elif (n<=2000):
+        print("the number is between 1000 and 2000.")
+        
+
+number(int(input("Enter the number:")))
+    
+            
