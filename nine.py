cadb18548ee176c2abdc1aae1033f90c03d53f43
priya <shunmugapriya530@gmail.com>
2023-03-25 07:55:23 +0530

    my program

--- nine.py
+++ nine.py
@@ -1,19 +1,7 @@
 print("\t\tLetter is a vowel or not :\t\t")
 choice=input("Enter your choice :")
 if choice in ["a","e","i","o","u"]:
-    if  choice=="a":
-        print("Letter is vowel")
-    elif choice=="e":
-        print("Letter is Vowel")
-    elif choice=="i":
-        print("Letter is vowel")
-    elif choice=="i":
-        print("Letter is vowel")
-    elif choice=="o":
-        print("Letter is vowel")
-    elif choice=="u":
-        print("Letter is vowel")
-        
+    print("Letter is a vowel")        
 else:
     print("Letter is not a vowel!!") 
     
