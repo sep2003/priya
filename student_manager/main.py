14fa37ab6a2b25fcd1bbdd283a27bd938b1cb6d7
priya <shunmugapriya530@gmail.com>
2023-04-16 16:16:59 +0530

    my program

--- student_manager/main.py
+++ student_manager/main.py
@@ -1,13 +1,17 @@
 import student_man
 while True:
     print("Welcome to the application !!")
-    print("1.Registration ")
-    choice=input("Enter the choice[1] : ")
+    print("1.Registration ","2.View student")
+    choice=input("Enter the choice[1,2] : ")
     if choice=="1":
         print("you have chosen thr registration method !!")
         print("Welcome to register the form..")
         student_man.reg()
         print("student register succesfully !!!")
+    elif choice=="2":
+        print("You have chosen the view student !!")
+        print("View Student")
+        student_man.student_view()
     else:
         print("Invalid choice!!")
         
--- student_manager/student_man.py
+++ student_manager/student_man.py
@@ -1,4 +1,6 @@
 import json
+import tabulate
+
 def reg():
     name=input("Enter your name : ")
     age=input("Enter your age : ")
@@ -15,5 +17,21 @@ def reg():
     fw=open("reg.json","w")
     json.dump(data,fw,indent=4)
     fw.close()
+    
+def student_view():
+    heading=["Name", "Age", "Duration"]
+    f=open("reg.json","r")
+    data=json.load(f)
+    f.close()
+    table=[]
+    
+    
+    for student_data in data["student_data"]:
+        temp_list=[student_data['name'],student_data['age'],student_data['duration']]
+        table.append(temp_list)
+    print(tabulate.tabulate(table,headers=heading))
+    #  print(f"{student_data['name']}\t{student_data['age']}\t{student_data['duration']}")
+   
+        
     
     
