9dbaf90ec1656340f96a43b886d38a51145f9655
priya <shunmugapriya530@gmail.com>
2023-04-19 08:52:29 +0530

    my program

--- student_manager/main.py
+++ student_manager/main.py
@@ -1,8 +1,8 @@
 import student_man
 while True:
     print("Welcome to the application !!")
-    print("1.Registration ","2.View student")
-    choice=input("Enter the choice[1,2] : ")
+    print("1.Registration\n","2.View student\n","3.Student update\n","4.Student delete ")
+    choice=input("Enter the choice[1,2,3,4] : ")
     if choice=="1":
         print("you have chosen thr registration method !!")
         print("Welcome to register the form..")
@@ -12,6 +12,14 @@ while True:
         print("You have chosen the view student !!")
         print("View Student")
         student_man.student_view()
+    elif choice=="3":
+        print("you have choosen student updated!! ")
+        print("Student update")
+        student_man.update_student()
+    elif choice=="4":
+        print("You have choosen student delete!!")
+        print("Student Delete")
+        student_man.student_delete()
     else:
         print("Invalid choice!!")
         
--- student_manager/student_man.py
+++ student_manager/student_man.py
@@ -19,19 +19,73 @@ def reg():
     fw.close()
     
 def student_view():
-    heading=["Name", "Age", "Duration"]
+    heading=["S.no","Name", "Age", "Duration"]
     f=open("reg.json","r")
     data=json.load(f)
     f.close()
     table=[]
+    s_no=1
     
     
     for student_data in data["student_data"]:
-        temp_list=[student_data['name'],student_data['age'],student_data['duration']]
+        temp_list=[s_no,student_data['name'],student_data['age'],student_data['duration']]
         table.append(temp_list)
+        s_no+=1
     print(tabulate.tabulate(table,headers=heading))
+    
     #  print(f"{student_data['name']}\t{student_data['age']}\t{student_data['duration']}")
-   
-        
+def update_student():
+    student_view()
+    f=open("reg.json","r")
+    data=json.load(f)
+    f.close()
+    stu_choice=input("Enter the s.no of the student you want to update :" )
+    print("you can update \n1.Name \n2.Age \n 3. Duration\n 4. All")
+    update_choice=input("please enter your choice : ")
+    sno=1
+    for student in data["student_data"]:
+        if str(sno)==stu_choice:
+            if update_choice=="1":
+                name=input("Enter update name : ")
+                student["name"]=name
+                print("Student data updated sucessfully!!")
+            elif update_choice=="2":
+                age=input("Enter updated age : ")
+                student["age"]=age
+                print("Student data updated sucessfully!! ")
+            elif update_choice==3:
+                duration=input("Enter updated duration: ")   
+                student["duration"]=duration
+                print("Student data updated sucessfully!!")
+            elif update_choice=="4":
+                student["name"]=input("Enter updated name : ")
+                student["age"]=input("Enter updated age :")
+                student["duration"]=input("Enter updated duration: ")
+                print("Student data updated sucessfully")  
+            else:
+                print("Invalid choice !!")
+                break
+        sno+=1 
+    f=open("reg.json","w") 
+    json.dump(data,f,indent=3)
+    f.close()  
+    
+def student_delete():
+    student_view()
+    f=open("reg.json","r")
+    data=json.load(f)
+    f.close()
+    stu_choice=input("Enter the s.no of the student you want to delete:" )
+    sno=1
+    for student in data["student_data"]:
+        if str(sno)==stu_choice:
+            data["student_data"].remove(student)
+            print("student data deleted sucessfully !!")
+        sno+=1
+    f=open("reg.json","w")
+    json.dump(data,f,indent=3)
+    f.close()
+            
+    
     
     
