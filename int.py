3646a1703c01e5a0f0f0ef811471307256e92885
unknown <shunmugapriya530@gamil.com>
2023-04-26 20:17:38 -0700

    my program

--- int.py
+++ int.py
@@ -1,9 +1,8 @@
-age=(input("Enter the age : "))
 name=(input("Enter the name : "))
+age=(input("Enter the age : "))
 print(age)
 print(name)
 print("My name is " +name)
 print("My age is " +age)
 print("I am "+name)
 print("I am not like my age"+age)
-
